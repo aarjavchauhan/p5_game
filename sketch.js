216a2a48ad7a21e5ea15440814390a8335455cd4
Aarjav Chauhan <aarjavchauhan439@gmail.com>
2019-09-25 22:50:50 -0600

    created ball and moving walls that close in

--- sketch.js
+++ sketch.js
@@ -1,7 +1,45 @@
+let leftWallStart = 0
+let rightWallStart = 0
+
 function setup() {
-  createCanvas(400, 400);
+  let mouseColor = color(139,0,0)
+  noCursor()
+  createCanvas(500, 500);
+  ball = new MainBall();
 }
 
 function draw() {
-  background(220);
+  background(0);
+  drawWall()
+  ball.display()
+  fill(0, 102, 153);
+  text(mouseX + '--' + mouseY, mouseX, mouseY);
+}
+
+class MainBall {
+  constructor() {
+    this.size = 25;
+  }
+  display(){
+    push()
+    noStroke()
+    fill(255)
+    ellipse(mouseX, mouseY, this.size, this.size)
+    pop()
+  }
+}
+
+function drawWall(){
+  push()
+  rectMode(CORNERS)
+  noStroke()
+  fill(221)
+  rect(0, 0, leftWallStart, height)
+  fill(139, 0, 120)
+  rect(width, 0, width-rightWallStart, height)
+  if(leftWallStart<width/2 || rightWallStart<width/2){
+    leftWallStart++
+    rightWallStart++
+  }
+  pop()
 }
